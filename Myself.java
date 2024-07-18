34ee7c1fb532190142a4f819954e84b7270379f4
boinashiva <boinashiva333@gmail.com>
2024-07-18 20:55:21 +0530

    Update Myself.java

--- Myself.java
+++ Myself.java
@@ -3,15 +3,15 @@ package firstproject;
 public class Myself {
 	public static void main(String[] args) {
 		// print 10 sentence about my self
-		System.out.println("my name is Supraja");
-		System.out.println("I am from Warangal");
+		System.out.println("my name is Shiva");
+		System.out.println("I am from suryapet");
 		System.out.println("I am studying at avanthi pg college");
-		System.out.println("I m Good learner");
+		System.out.println("I am Good learner");
 		System.out.println("virat kohli is the my favourate cricketer");
 		System.out.println("present living at hyderabad");
 		System.out.println("my favourate food is chicken");
 		System.out.println("my aim is becoming a software employe");
-		System.out.println("my best freind is supraja");
+		System.out.println("my best freind is sai");
 		System.out.println("I beleive my self");
 	}
 }
